5549d28e69a2ed25fda1a6b8168ff5a1476acc73
artur <artur@vis-design.com>
2016-04-08 17:27:28 +0300

    check config params

--- src/Http/route_frontend.php
+++ src/Http/route_frontend.php
@@ -4,53 +4,60 @@ $arrSegments = explode("/", Request::path());
 if ($arrSegments[0] != "admin") {
     try {
         $_model = Config::get('builder.tree.model');
-        $slug = end($arrSegments);
         $templates = Config::get('builder.tree.templates');
+        if ($_model) {
+            $slug = end ($arrSegments);
 
-        if (!$slug || $slug == LaravelLocalization::setLocale()) {
-            $slug = "/";
-        }
+            if (!$slug || $slug == LaravelLocalization::setLocale ()) {
+                $slug = "/";
+            }
 
-        $node = $_model::where("slug", 'like', $slug)->first();
+            $node = $_model::where ("slug", 'like', $slug)->first ();
 
-        if (isset($node->id)) {
-            $slugTree = $slug;
-            $_nodeUrl = trim($node->getUrlNoLocation(), "/");
-            Route::group(['middleware' => ['web']], function () use ($node, $_nodeUrl, $templates){
-                Route::group (['prefix' => LaravelLocalization::setLocale ()],
+            if (isset($node->id)) {
+                $slugTree = $slug;
+                $_nodeUrl = trim ($node->getUrlNoLocation (), "/");
+                Route::group (['middleware' => ['web']],
                     function () use ($node, $_nodeUrl, $templates) {
+                        Route::group (['prefix' => LaravelLocalization::setLocale ()],
+                            function () use ($node, $_nodeUrl, $templates) {
 
-                        Route::get ($_nodeUrl,
-                            function() use ($node, $templates) {
+                                Route::get ($_nodeUrl,
+                                    function () use ($node, $templates) {
 
-                                if (!isset($templates[$node->template])) {
-                                    App::abort(404);
-                                }
+                                        if (!isset($templates[$node->template])) {
+                                            App::abort (404);
+                                        }
 
-                                list($controller, $method) = explode ('@', $templates[$node->template]['action']);
+                                        list($controller, $method)
+                                            = explode ('@',
+                                            $templates[$node->template]['action']);
 
-                                $app = app();
-                                $controller = $app->make("App\\Http\\Controllers\\".$controller);
+                                        $app = app ();
+                                        $controller
+                                            = $app->make ("App\\Http\\Controllers\\"
+                                            . $controller);
 
-                                return $controller->callAction ('init', array ($node, $method));
+                                        return $controller->callAction ('init',
+                                            array ($node, $method));
+                                    });
+
                             });
-
                     });
-            });
 
-            if (LaravelLocalization::setLocale() == "") {
-                $pathUrl = "/" . Request::path();
-            } else {
-                $pathUrl = Request::path();
-            }
+                if (LaravelLocalization::setLocale () == "") {
+                    $pathUrl = "/" . Request::path ();
+                } else {
+                    $pathUrl = Request::path ();
+                }
 
-            if ($pathUrl == LaravelLocalization::setLocale() . $_nodeUrl) {
-                Session::put('currentNode', $node);
-            } else {
-                Session::put('currentNode', $node);
+                if ($pathUrl == LaravelLocalization::setLocale () . $_nodeUrl) {
+                    Session::put ('currentNode', $node);
+                } else {
+                    Session::put ('currentNode', $node);
+                }
             }
         }
-
 
     } catch (Exception $e) { }
 
--- src/Http/routers.php
+++ src/Http/routers.php
@@ -1,167 +1,171 @@
 <?php
+$menuLinks = config('builder.admin.menu');
 
-$all_links = array_column(config('builder.admin.menu'), "link");
+if ($menuLinks) {
+    $all_links = array_column ($menuLinks, "link");
 
-foreach (config('builder.admin.menu') as $menu) {
-    if (is_array($menu)) {
-        foreach($menu as $menuLink) {
-            if (is_array($menuLink)) {
-                $all_links[] = array_column($menuLink, "link");
+    foreach (config ('builder.admin.menu') as $menu) {
+        if (is_array ($menu)) {
+            foreach ($menu as $menuLink) {
+                if (is_array ($menuLink)) {
+                    $all_links[] = array_column ($menuLink, "link");
+                }
             }
+
         }
-
     }
-}
 
-$all_links = array_flatten($all_links);
-$all_links_str = implode("|",$all_links);
-$all_links_str = str_replace("/","",$all_links_str);
+    $all_links = array_flatten ($all_links);
+    $all_links_str = implode ("|", $all_links);
+    $all_links_str = str_replace ("/", "", $all_links_str);
 
-Route::pattern('page_admin', $all_links_str);
-Route::pattern('tree_name', '[a-z0-9-_]+');
-Route::pattern('any', '[a-z0-9-_/\]+');
-Route::group(['middleware' => ['web']], function () {
-    Route::group (
-        ['prefix' => 'admin', 'middleware' => 'auth.admin'],
-        function () {
+    Route::pattern ('page_admin', $all_links_str);
+    Route::pattern ('tree_name', '[a-z0-9-_]+');
+    Route::pattern ('any', '[a-z0-9-_/\]+');
+    Route::group (['middleware' => ['web']], function () {
+        Route::group (
+            ['prefix' => 'admin', 'middleware' => 'auth.admin'],
+            function () {
 
-            Route::any ('/tree', 'Vis\Builder\TableAdminController@showTree');
-            Route::any ('/handle/tree',
-                'Vis\Builder\TableAdminController@handleTree');
+                Route::any ('/tree',
+                    'Vis\Builder\TableAdminController@showTree');
+                Route::any ('/handle/tree',
+                    'Vis\Builder\TableAdminController@handleTree');
 
-            Route::any ('/{tree_name}_tree',
-                'Vis\Builder\TableAdminController@showTreeOther');
-            Route::any ('/handle/{tree_name}_tree',
-                'Vis\Builder\TableAdminController@handleTreeOther');
+                Route::any ('/{tree_name}_tree',
+                    'Vis\Builder\TableAdminController@showTreeOther');
+                Route::any ('/handle/{tree_name}_tree',
+                    'Vis\Builder\TableAdminController@handleTreeOther');
 
-            Route::post ('/show_all_tree/{tree_name}',
-                'Vis\Builder\TableAdminController@showTreeAll');
+                Route::post ('/show_all_tree/{tree_name}',
+                    'Vis\Builder\TableAdminController@showTreeAll');
 
-            //router for pages builder
-            Route::get ('/{page_admin}',
-                'Vis\Builder\TableAdminController@showPage');
-            Route::post ('/{page_admin}',
-                'Vis\Builder\TableAdminController@showPagePost');
+                //router for pages builder
+                Route::get ('/{page_admin}',
+                    'Vis\Builder\TableAdminController@showPage');
+                Route::post ('/{page_admin}',
+                    'Vis\Builder\TableAdminController@showPagePost');
 
-            Route::post ('/handle/{page_admin}',
-                'Vis\Builder\TableAdminController@handlePage');
+                Route::post ('/handle/{page_admin}',
+                    'Vis\Builder\TableAdminController@handlePage');
 
-            // docs page
-            Route::get ('/', 'Vis\Builder\TBController@showDashboard');
+                // docs page
+                Route::get ('/', 'Vis\Builder\TBController@showDashboard');
 
-            // logout
-            Route::get ('logout', array (
-                    'as' => 'logout',
-                    'uses' => 'Vis\Builder\LoginController@doLogout'
-                )
-            );
+                // logout
+                Route::get ('logout', array (
+                        'as' => 'logout',
+                        'uses' => 'Vis\Builder\LoginController@doLogout'
+                    )
+                );
 
-            //routes for froala editor
-            Route::post ('upload_image', array (
-                    'as' => 'upload_image',
-                    'uses' => 'Vis\Builder\EditorController@uploadFoto'
-                )
-            );
-            Route::post ('upload_file', array (
-                    'as' => 'upload_file',
-                    'uses' => 'Vis\Builder\EditorController@uploadFile'
-                )
-            );
-            Route::get ('load_image', array (
-                    'as' => 'load_image',
-                    'uses' => 'Vis\Builder\EditorController@loadImages'
-                )
-            );
-            Route::post ('delete_image', array (
-                    'as' => 'delete_image',
-                    'uses' => 'Vis\Builder\EditorController@deleteImages'
-                )
-            );
+                //routes for froala editor
+                Route::post ('upload_image', array (
+                        'as' => 'upload_image',
+                        'uses' => 'Vis\Builder\EditorController@uploadFoto'
+                    )
+                );
+                Route::post ('upload_file', array (
+                        'as' => 'upload_file',
+                        'uses' => 'Vis\Builder\EditorController@uploadFile'
+                    )
+                );
+                Route::get ('load_image', array (
+                        'as' => 'load_image',
+                        'uses' => 'Vis\Builder\EditorController@loadImages'
+                    )
+                );
+                Route::post ('delete_image', array (
+                        'as' => 'delete_image',
+                        'uses' => 'Vis\Builder\EditorController@deleteImages'
+                    )
+                );
 
-            Route::post ('quick_edit', array (
-                    'as' => 'quick_edit',
-                    'uses' => 'Vis\Builder\EditorController@doQuickEdit'
-                )
-            );
+                Route::post ('quick_edit', array (
+                        'as' => 'quick_edit',
+                        'uses' => 'Vis\Builder\EditorController@doQuickEdit'
+                    )
+                );
 
-            //change skin for admin panel
-            Route::post ('change_skin', array (
-                    'as' => 'change_skin',
-                    'uses' => 'Vis\Builder\TBController@doChangeSkin'
-                )
-            );
+                //change skin for admin panel
+                Route::post ('change_skin', array (
+                        'as' => 'change_skin',
+                        'uses' => 'Vis\Builder\TBController@doChangeSkin'
+                    )
+                );
 
-            Route::get ('change_lang', array (
-                    'as' => 'change_lang',
-                    'uses' => 'Vis\Builder\TBController@doChangeLangAdmin'
-                )
-            );
+                Route::get ('change_lang', array (
+                        'as' => 'change_lang',
+                        'uses' => 'Vis\Builder\TBController@doChangeLangAdmin'
+                    )
+                );
 
-            Route::any ('/photos/gallery', array (
-                    'as' => 'photos_all',
-                    'uses' => 'Vis\Builder\PhotosController@fetchShowAll'
-                )
-            );
-            Route::post ('/photos/save_album', array (
-                    'as' => 'save_album',
-                    'uses' => 'Vis\Builder\PhotosController@doSaveAlbum'
-                )
-            );
+                Route::any ('/photos/gallery', array (
+                        'as' => 'photos_all',
+                        'uses' => 'Vis\Builder\PhotosController@fetchShowAll'
+                    )
+                );
+                Route::post ('/photos/save_album', array (
+                        'as' => 'save_album',
+                        'uses' => 'Vis\Builder\PhotosController@doSaveAlbum'
+                    )
+                );
 
-            Route::get ('/load_view_statistic', array (
-                    'as' => 'load_view_statistic',
-                    'uses' => 'Vis\Builder\ViewStatisticController@getStatistic'
-                )
-            );
+                Route::get ('/load_view_statistic', array (
+                        'as' => 'load_view_statistic',
+                        'uses' => 'Vis\Builder\ViewStatisticController@getStatistic'
+                    )
+                );
 
-        });
-});
+            });
+    });
 
-// login post
+    // login post
 
-Route::group(['middleware' => ['web']], function () {
-    Route::get('login',  array(
-            'as' => 'login_show',
-            'uses' => 'Vis\Builder\LoginController@showLogin')
-    );
-    Route::post('login',  array(
-            'as' => 'login',
-            'uses' => 'Vis\Builder\LoginController@postLogin')
-    );
+    Route::group (['middleware' => ['web']], function () {
+        Route::get ('login', array (
+                'as' => 'login_show',
+                'uses' => 'Vis\Builder\LoginController@showLogin'
+            )
+        );
+        Route::post ('login', array (
+                'as' => 'login',
+                'uses' => 'Vis\Builder\LoginController@postLogin'
+            )
+        );
 
         /*
      * routes for correct works debugbar
      */
-        Route::get('/_debugbar/assets/stylesheets', [
+        Route::get ('/_debugbar/assets/stylesheets', [
             'as' => 'debugbar-css',
             'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
         ]);
 
-        Route::get('/_debugbar/assets/javascript', [
+        Route::get ('/_debugbar/assets/javascript', [
             'as' => 'debugbar-js',
             'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
         ]);
 
-        Route::get('/_debugbar/open', [
+        Route::get ('/_debugbar/open', [
             'as' => 'debugbar-open',
             'uses' => '\Barryvdh\Debugbar\Controllers\OpenController@handler'
         ]);
-});
-//login show
+    });
+    //login show
 
 
+    if (Request::ajax () && Request::is ('admin/*')) {
+        /* App::error(function (\Exception $exception, $code) {
+             return Vis\Builder\TBController::returnError($exception, $code);
+         });*/
+        /*  $this->app->error(function(Exception $e) {
+              dd('foo');
+          });*/
+    } else {
 
-if (Request::ajax() && Request::is('admin/*')) {
-   /* App::error(function (\Exception $exception, $code) {
-        return Vis\Builder\TBController::returnError($exception, $code);
-    });*/
-  /*  $this->app->error(function(Exception $e) {
-        dd('foo');
-    });*/
-} else {
-
-    Route::any('{catchall}', function() {
-        return Response::view('admin::errors.404', array(), 404);
-    })->where('catchall', '.*');
+        Route::any ('{catchall}', function () {
+            return Response::view ('admin::errors.404', array (), 404);
+        })->where ('catchall', '.*');
+    }
 }
-
