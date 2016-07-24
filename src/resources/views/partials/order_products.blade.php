8ae30cd0b9f08ca3c6a5171e30fdb3ff39b059c1
artur <artur@vis-design.com>
2016-07-24 13:28:11 +0300

    fix style

--- src/resources/views/partials/order_products.blade.php
+++ src/resources/views/partials/order_products.blade.php
@@ -1,33 +1,40 @@
 @if(isset($orderProducts) && count($orderProducts))
-
+    <style>
+        .table_products_in_order{
+            width: 100%;
+        }
+        .table_products_in_order td{
+            padding: 5px;
+        }
+    </style>
     <table class="table_products_in_order">
         <thead>
-            <tr>
-                <td>#</td>
-                <td>Название</td>
-                <td>Кол-во, шт.</td>
-                <td>Цена, грн</td>
-                @if(OrderProduct::$showParam == true)
-                    <td>Параметры</td>
-                @endif
-            </tr>
+        <tr>
+            <td style="width: 10px">#</td>
+            <td>{{__('Название')}}</td>
+            <td style="width: 80px">{{__('Кол-во, шт.')}}</td>
+            <td style="width: 60px">{{__('Цена, грн')}}</td>
+            @if(OrderProduct::$showParam == true)
+                <td>{{__('Параметры')}}</td>
+            @endif
+        </tr>
         </thead>
         <tbody>
-            @foreach($orderProducts as $product)
+        @foreach($orderProducts as $product)
             <? $tovar = $product->getTovar(); ?>
-              <tr>
+            <tr>
                 <td>{{$loop->index1}}</td>
                 <td>
-                <a href="{{$tovar->getUrl()}}" target="_blank">{{$tovar->getImg("50", "50")}}</a>
-                <a href="{{$tovar->getUrl()}}" target="_blank">{{$tovar->title}}</a></td>
+                    <a href="{{$tovar->getUrl()}}" target="_blank">{!! $tovar->getImg("50", "50")!!}</a>
+                    <a href="{{$tovar->getUrl()}}" target="_blank">{{$tovar->title}}</a></td>
                 <td>{{$product->count}}</td>
                 <td>{{$product->price}}</td>
                 @if(OrderProduct::$showParam == true)
                     <td>{{$product->params}}</td>
                 @endif
-             </tr>
-            @endforeach
+            </tr>
+        @endforeach
         </tbody>
     </table>
 
-@endif
+    @endiff
