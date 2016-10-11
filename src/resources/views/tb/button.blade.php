dc3ee161b23d574429b16ca3ba6f5053e63f9f04
artur <artur@vis-design.com>
2016-10-11 17:15:51 +0300

    fix button and add ajax

--- src/resources/views/tb/button.blade.php
+++ src/resources/views/tb/button.blade.php
@@ -1,6 +1,29 @@
 <div class="widget-toolbar" role="menu">
-    <a href="{{$button['link']}}" class="btn dropdown-toggle btn-xs btn-default"  data-toggle="dropdown">
+    @if (isset($button['ajax']))
+        <a onclick="sendAjax()" class="btn btn-xs btn-default">
+            <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
+            {{__cms($button['caption'])}}
+        </a>
+
+        <script>
+            if (typeof sendAjax != 'function') {
+                function sendAjax() {
+                    $.post("{{$button['link']}}", {},
+                            function(data){
+                                if (data.status == 'success') {
+                                    TableBuilder.showSuccessNotification(data.message);
+                                } else {
+                                    TableBuilder.showErrorNotification(data.message);
+                                }
+                            }, 'json');
+                }
+            }
+            
+        </script>
+    @else
+    <a href="{{$button['link']}}" class="btn btn-xs btn-default">
         <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
         {{__cms($button['caption'])}}
     </a>
+    @endif
 </div>
