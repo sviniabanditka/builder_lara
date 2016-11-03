f76e4475ebf207d7a4f5628920f0309c571fcea5
s_litvin <litvin.xx@gmail.com>
2016-11-03 11:57:39 +0200

    allow add foreign fields values

--- src/resources/views/tb/input_foreign.blade.php
+++ src/resources/views/tb/input_foreign.blade.php
@@ -1,10 +1,30 @@
+
+@if ($allow_foreign_add)
+<script>
+    $(document).ready(function () {
+        $('label[for="{{ $name }}"]').append(' | <a href="" class="{{ $name }}_new_toggle fa fa-plus"></a>');
+        $('.{{ $name }}_new_toggle').click(function(e) {
+            e.preventDefault();
+            var new_f_selector = $('.{{ $name }}_add_new_foreign');
+            new_f_selector.slideToggle("fast", function () {
+                if (!new_f_selector.is(":visible")) {
+                    $('input[name={{ $name }}_new]').val('');
+                }
+            });
+            $('.{{ $name }}_foreign').slideToggle("fast");
+        });
+    });
+</script>
+@endif
+
+
 <select
 
  @if (Input::has("id") && $readonly_for_edit)
      disabled
  @endif
 
- name="{{ $name }}" class="dblclick-edit-input form-control input-small unselectable">
+ name="{{ $name }}" class="dblclick-edit-input form-control input-small unselectable {{ $name }}_foreign">
     @if ($is_null)
         <option value="">{{ $null_caption ? : '...' }}</option>
     @endif
@@ -20,3 +40,9 @@
     @endif
 
 </select>
+
+@if ($allow_foreign_add)
+<div class="{{ $name }}_add_new_foreign" style="display: none">
+    <input type="text" name="{{ $name }}_new_foreign" placeholder="Добавить новое значение" />
+</div>
+@endif
