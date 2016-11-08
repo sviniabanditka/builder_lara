881b7b461f25fc9bf89a845542aeb161a55caae1
artur <artur@vis-design.com>
2016-11-08 13:19:57 +0100

    add tab

--- src/resources/views/tb/group_access_list.blade.php
+++ src/resources/views/tb/group_access_list.blade.php
@@ -1,13 +1,56 @@
 @if (isset($permissions) && count($permissions))
     @foreach($permissions as $permissionAlias => $permission)
-        <p><label class="checkbox">
-                <input type="checkbox" value="1" name="permissions[{{$permissionAlias}}]"
-                @if (isset($groupPermissionsThis[$permissionAlias]) && $groupPermissionsThis[$permissionAlias])
-                    checked
+
+        @if ($permissionAlias == 'tabs')
+
+            @foreach($permission as $permissionOneName => $permissionOne)
+                <section style="float: left; padding-right: 20px">
+                    @if (is_array($permissionOne))
+                        <label class="label">{{$permissionOneName}}</label>
+                        @foreach($permissionOne as $permissionAlias2 => $permission2)
+                            <p><label class="checkbox">
+                                    <input type="checkbox" value="1" name="permissions[{{$permissionAlias2}}]"
+                                           @if (isset($groupPermissionsThis[$permissionAlias2]) && $groupPermissionsThis[$permissionAlias2])
+                                           checked
+                                            @endif
+                                    >
+                                    <i></i> {{$permission2}}
+                                </label>
+                            </p>
+                        @endforeach
+                    @endif
+                </section>
+            @endforeach
+
+        @else
+
+            <section class="">
+                @if (is_array($permission))
+                    <label class="label">{{$permissionAlias}}</label>
+                    @foreach($permission as $permissionAlias2 => $permission2)
+                        <p><label class="checkbox">
+                                <input type="checkbox" value="1" name="permissions[{{$permissionAlias2}}]"
+                                       @if (isset($groupPermissionsThis[$permissionAlias2]) && $groupPermissionsThis[$permissionAlias2])
+                                       checked
+                                        @endif
+                                >
+                                <i></i> {{$permission2}}
+                            </label>
+                        </p>
+                    @endforeach
+                @else
+
+                    <p><label class="checkbox">
+                            <input type="checkbox" value="1" name="permissions[{{$permissionAlias}}]"
+                            @if (isset($groupPermissionsThis[$permissionAlias]) && $groupPermissionsThis[$permissionAlias])
+                                checked
+                            @endif
+                            >
+                            <i></i> {{$permission}}
+                       </label>
+                    </p>
                 @endif
-                >
-                <i></i> {{$permission}}
-           </label>
-        </p>
+            </section>
+        @endif
     @endforeach
 @endif
