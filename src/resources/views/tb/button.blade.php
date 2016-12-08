df30d8d4bbfccbc9521dcd19f4d9a9934838e0c9
artur <artur@vis-design.com>
2016-12-08 13:57:49 +0100

    fix button link

--- src/resources/views/tb/button.blade.php
+++ src/resources/views/tb/button.blade.php
@@ -1,14 +1,14 @@
 <div class="widget-toolbar" role="menu">
     @if (isset($button['ajax']))
-        <a onclick="sendAjax()" class="btn btn-xs btn-default">
+        <a onclick="sendAjax('{{$button['link']}}')" class="btn btn-xs btn-default">
             <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
             {{__cms($button['caption'])}}
         </a>
 
         <script>
             if (typeof sendAjax != 'function') {
-                function sendAjax() {
-                    $.post("{{$button['link']}}", {},
+                function sendAjax(link) {
+                    $.post(link, {},
                         function(data){
                             if (data.status == 'success') {
                                 TableBuilder.showSuccessNotification(data.message);
