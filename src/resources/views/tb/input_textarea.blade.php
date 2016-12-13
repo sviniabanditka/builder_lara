e75e6ba8f39f287d7dbce2579b5624390e696693
artur <artur@vis-design.com>
2016-12-13 13:09:42 +0200

    fix label

--- src/resources/views/tb/input_textarea.blade.php
+++ src/resources/views/tb/input_textarea.blade.php
@@ -1,7 +1,7 @@
-<textarea rows="{{$rows or '3'}}"
+<label class="textarea"><textarea rows="{{$rows or '3'}}"
           class="custom-scroll"
           id="{{$name}}"
-          name="{{ $name }}">{{ $value }}</textarea>
+          name="{{ $name }}">{{ $value }}</textarea></label>
 @if (isset($comment) && $comment)
   <div class="note">
       {{$comment}}
