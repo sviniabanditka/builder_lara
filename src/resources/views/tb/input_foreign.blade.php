fe80ff7726196810ffb66d8ee3c51f5fe3e35e83
artur <artur@vis-design.com>
2017-01-11 13:08:10 +0200

    fix style

--- src/resources/views/tb/input_foreign.blade.php
+++ src/resources/views/tb/input_foreign.blade.php
@@ -17,30 +17,31 @@
 </script>
 @endif
 
+<label class="select">
+    <select
 
-<select
+     @if (Input::has("id") && $readonly_for_edit)
+         disabled
+     @endif
 
- @if (Input::has("id") && $readonly_for_edit)
-     disabled
- @endif
+     name="{{ $name }}" class="dblclick-edit-input form-control input-small unselectable {{ $name }}_foreign">
+        @if ($is_null)
+            <option value="">{{ $null_caption ? : '...' }}</option>
+        @endif
 
- name="{{ $name }}" class="dblclick-edit-input form-control input-small unselectable {{ $name }}_foreign">
-    @if ($is_null)
-        <option value="">{{ $null_caption ? : '...' }}</option>
-    @endif
+        @if ($recursive)
+             @foreach ($options as $value => $caption)
+                  {!! $caption !!}
+             @endforeach
+        @else
+             @foreach ($options as $value => $caption)
+                   <option value="{{ $value }}" {{$value == $selected ? "selected" : ""}} >{{ __cms($caption) }}</option>
+             @endforeach
+        @endif
 
-    @if ($recursive)
-         @foreach ($options as $value => $caption)
-              {!! $caption !!}
-         @endforeach
-    @else
-         @foreach ($options as $value => $caption)
-               <option value="{{ $value }}" {{$value == $selected ? "selected" : ""}} >{{ __cms($caption) }}</option>
-         @endforeach
-    @endif
-
-</select>
-
+    </select>
+    <i></i>
+</label>
 @if ($allow_foreign_add)
 <div class="{{ $name }}_add_new_foreign" style="display: none">
     <input type="text" name="{{ $name }}_new_foreign" placeholder="Добавить новое значение" />
