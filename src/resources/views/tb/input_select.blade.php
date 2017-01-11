a7fc309efa8ca2caba6b597839ac87669eb015cc
artur <artur@vis-design.com>
2017-01-11 12:34:30 +0200

    fix style

--- src/resources/views/tb/input_select.blade.php
+++ src/resources/views/tb/input_select.blade.php
@@ -1,18 +1,21 @@
-<select
+<label class="select">
+    <select
 
-@if (Input::has("id") && $readonly_for_edit)
-    disabled
-@endif
+    @if (Input::has("id") && $readonly_for_edit)
+        disabled
+    @endif
 
-name="{{ $name }}" class="dblclick-edit-input form-control input-small unselectable {{$action ? "action" : ""}}">
-    @foreach ($options as $value => $caption)
-        @if ($value == $selected)
-            <option value="{{ $value }}" selected>{{ __cms($caption) }}</option>
-        @else
-            <option value="{{ $value }}">{{ __cms($caption) }}</option>
-        @endif
-    @endforeach
-</select>
+    name="{{ $name }}" class="dblclick-edit-input form-control input-small unselectable {{$action ? "action" : ""}}">
+        @foreach ($options as $value => $caption)
+            @if ($value == $selected)
+                <option value="{{ $value }}" selected>{{ __cms($caption) }}</option>
+            @else
+                <option value="{{ $value }}">{{ __cms($caption) }}</option>
+            @endif
+        @endforeach
+    </select>
+    <i></i>
+</label>
 @if (isset($comment) && $comment)
   <div class="note">
       {{$comment}}
