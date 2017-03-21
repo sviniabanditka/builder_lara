b61bc4f422542700357a07589b6e0013a4b28820
artur <artur@vis-design.com>
2017-03-21 10:59:57 +0100

    fi double send form

--- src/resources/views/tb/filter_text.blade.php
+++ src/resources/views/tb/filter_text.blade.php
@@ -2,7 +2,7 @@
 <div style="position: relative;">
     <input type="text" value="{{ $value }}" name="filter[{{ $name }}]" class="form-control input-small" />
     @if ($value)
-    <button onclick="$(this).parent().find('input').val(''); setTimeout(function(){ TableBuilder.search(); }, 200);" class="close" style="position: absolute; top: 8px; right: 6px;">
+    <button onclick="$(this).parent().find('input').val(''); setTimeout(function(){ TableBuilder.search(); }, 200); return false;" class="close" style="position: absolute; top: 8px; right: 6px;">
         ×
     </button>
     @endif
