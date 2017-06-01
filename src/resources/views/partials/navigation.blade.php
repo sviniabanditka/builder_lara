39b8bca9b296acf1724cb579ea10c610af7d655c
artur <artur@vis-design.com>
2017-06-01 18:18:03 +0300

    add badge

--- src/resources/views/partials/navigation.blade.php
+++ src/resources/views/partials/navigation.blade.php
@@ -14,74 +14,82 @@
     <nav>
         <ul style="display: block;">
             @foreach($menu as $k=>$el)
-              @if(!isset($el['check']) || $el['check']())
+                @if(!isset($el['check']) || $el['check']())
 
-                 @if (isset($el['custom']))
-                     {!! $el['custom']() !!}
-                 @else
+                    @if (isset($el['custom']))
+                        {!! $el['custom']() !!}
+                    @else
 
-                     <li class="level1">
-                         <a  {!! isset($el['link']) && !isset($el['submenu'])? "href='/admin".$el['link']."'" : "" !!}>
-                            @if (isset($el['icon']))
-                                <i class="fa fa-lg fa-fw fa-{{$el['icon']}}"></i>
-                            @endif
-                            @if (isset($el['title']))
-                            <span class="menu-item-parent">{{__cms($el['title'])}}</span>
-                            @endif
-                            @if (isset($el['badge']))
-                               <?php $countBadge = $el['badge'](); ?>
-                               @if (is_numeric($countBadge))
-                                <span class="badge bg-color-greenLight pull-right inbox-badge">{{$countBadge}}</span>
-                               @endif
-                            @endif
-                         </a>
-
-                          @if(isset($el['submenu']))
-                           <ul>
-                              @foreach($el['submenu'] as $k_sub_menu=>$sub_menu)
-                                @if(!isset($sub_menu['check']) || $sub_menu['check']())
-                                    <li>
-                                        <a
-                                          {!! isset($sub_menu['link']) && !isset($sub_menu['submenu']) ? "href='/admin".$sub_menu['link']."'" : "" !!}
-                                        >{{__cms($sub_menu['title'])}}
-
-                                        @if (isset($sub_menu['badge']))
-                                           <?php $countBadge = $sub_menu['badge'](); ?>
-                                           @if (is_numeric($countBadge))
-                                            <span class="badge bg-color-greenLight pull-right inbox-badge">{{$countBadge}}</span>
-                                           @endif
-                                         @endif
-                                        </a>
-                                         @if(isset($sub_menu['submenu']))
-
-                                              <ul>
-                                                @foreach($sub_menu['submenu'] as $k_sub_menu2=>$sub_menu2)
-                                                   @if(!isset($sub_menu2['check']) || $sub_menu2['check']())
-                                                    <li>
-                                                        <a {!!isset($sub_menu2['link']) && !isset($sub_menu2['submenu']) ? "href='/admin".$sub_menu2['link']."'" : "" !!}>{{__cms($sub_menu2['title'])}}</a>
-
-                                                         @if (isset($sub_menu2['badge']))
-                                                           <?php $countBadge = $sub_menu2['badge'](); ?>
-                                                           @if (is_numeric($countBadge))
-                                                            <span class="badge bg-color-greenLight pull-right inbox-badge">{{$countBadge}}</span>
-                                                           @endif
-                                                         @endif
-                                                    </li>
-                                                   @endif
-                                                @endforeach
-                                              </ul>
-                                         @endif
-                                    </li>
+                        <li class="level1">
+                            <a  {!! isset($el['link']) && !isset($el['submenu'])? "href='/admin".$el['link']."'" : "" !!}>
+                                @if (isset($el['icon']))
+                                    <i class="fa fa-lg fa-fw fa-{{$el['icon']}}"></i>
                                 @endif
-                              @endforeach
-                            </ul>
-                         @endif
-                    </li>
-                 @endif
-              @endif
+                                @if (isset($el['title']))
+                                    <span class="menu-item-parent">{{__cms($el['title'])}}</span>
+                                @endif
+                                @if (isset($el['badge']))
+                                    <?php $badgeValue = $el['badge'](); ?>
+
+                                    <span
+                                            class="badge bg-color-greenLight pull-right inbox-badge"
+                                            style="@if(!$badgeValue) display: none @endif"
+                                    >
+                                        {{is_numeric($badgeValue) ? $badgeValue : ''}}
+                                    </span>
+                                @endif
+                            </a>
+
+                            @if(isset($el['submenu']))
+                                <ul>
+                                    @foreach($el['submenu'] as $k_sub_menu=>$sub_menu)
+                                        @if(!isset($sub_menu['check']) || $sub_menu['check']())
+                                            <li>
+                                                <a
+                                                        {!! isset($sub_menu['link']) && !isset($sub_menu['submenu']) ? "href='/admin".$sub_menu['link']."'" : "" !!}
+                                                >{{__cms($sub_menu['title'])}}
+
+                                                    @if (isset($sub_menu['badge']))
+                                                        <?php $badgeValue = $sub_menu['badge'](); ?>
+
+                                                        <span
+                                                                class="badge bg-color-greenLight pull-right inbox-badge"
+                                                                style="@if(!$badgeValue) display: none @endif"
+                                                        >
+                                        {{is_numeric($badgeValue) ? $badgeValue : ''}}
+                                    </span>
+                                                    @endif
+                                                </a>
+                                                @if(isset($sub_menu['submenu']))
+
+                                                    <ul>
+                                                        @foreach($sub_menu['submenu'] as $k_sub_menu2=>$sub_menu2)
+                                                            @if(!isset($sub_menu2['check']) || $sub_menu2['check']())
+                                                                <li>
+                                                                    <a {!!isset($sub_menu2['link']) && !isset($sub_menu2['submenu']) ? "href='/admin".$sub_menu2['link']."'" : "" !!}>{{__cms($sub_menu2['title'])}}</a>
+
+                                                                    @if (isset($sub_menu2['badge']))
+                                                                        <?php $countBadge = $sub_menu2['badge'](); ?>
+                                                                        @if (is_numeric($countBadge))
+                                                                            <span class="badge bg-color-greenLight pull-right inbox-badge">{{$countBadge}}</span>
+                                                                        @endif
+                                                                    @endif
+                                                                </li>
+                                                            @endif
+                                                        @endforeach
+                                                    </ul>
+                                                @endif
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </li>
+                    @endif
+                @endif
             @endforeach
         </ul>
-     </nav>
+    </nav>
     <span class="minifyme" data-action="minifyMenu">
         <i class="fa fa-arrow-circle-left hit"></i>
     </span>
