441132f3ece094c3336d39ea247ccb95c5a29a02
KHlushchenko <d2bl@ukr.net>
2017-08-30 14:37:13 +0300

    SlugHandler helper class for slug generation

---
+++ src/Http/Helpers/SlugHandler.php
@@ -0,0 +1,20 @@
+<?php namespace Vis\Builder\Helpers;
+
+use Vis\Builder\Handlers\CustomHandler;
+use Vis\Builder\Jarboe;
+
+class SlugHandler extends CustomHandler
+{
+    public function onInsertRowResponse(array &$response)
+    {
+        $model =  $this->controller->getDefinition()['options']['model'];
+        $model::where('id',$response['id'])->update(['slug' => Jarboe::urlify(($response['values']['title']))]);
+    } // end onInsertRowResponse
+    
+    public function onUpdateRowResponse(array &$response)
+    {
+        $model =  $this->controller->getDefinition()['options']['model'];
+        $model::where('id',$response['id'])->update(['slug' => Jarboe::urlify(($response['values']['title']))]);
+    }
+
+}
