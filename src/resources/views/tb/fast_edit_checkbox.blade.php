be331da67425d5b086f8d698d5b8964ffb2abf7f
artur <artur@vis-design.com>
2018-06-11 12:23:18 +0300

    fix unic id

--- src/resources/views/tb/fast_edit_checkbox.blade.php
+++ src/resources/views/tb/fast_edit_checkbox.blade.php
@@ -1,9 +1,10 @@
 <div style="position: relative">
      <span class="onoffswitch">
-        <input onchange="TableBuilder.activeToggle('{{$row['id']}}', '{{ $ident }}', this.checked);" type="checkbox" name="onoffswitch" class="onoffswitch-checkbox"  id="myonoffswitch{{$row['id']}}"
+        <input onchange="TableBuilder.activeToggle('{{$row['id']}}', '{{ $ident }}', this.checked);"
+               type="checkbox" name="onoffswitch" class="onoffswitch-checkbox"  id="myonoffswitch{{$row['id']}}_{{ $ident }}"
         {{ $row[$ident] ? 'checked' : '' }}
         >
-        <label class="onoffswitch-label" for="myonoffswitch{{$row['id']}}">
+        <label class="onoffswitch-label" for="myonoffswitch{{$row['id']}}_{{ $ident }}">
             <span class="onoffswitch-inner" data-swchon-text="ДА" data-swchoff-text="НЕТ"></span>
             <span class="onoffswitch-switch"></span>
         </label>
