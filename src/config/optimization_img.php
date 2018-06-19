d5a18335fb623a9b6a7d4d32177a54674ef37586
artur <artur@vis-design.com>
2018-06-19 09:52:37 +0300

    chnage method optimize

--- src/config/optimization_img.php
+++ src/config/optimization_img.php
@@ -2,6 +2,6 @@
 
 return [
     'active' => true,
-    'jpg_path' => 'jpegtran -copy none -optimize -progressive -outfile [file] [file]',
+    'jpg_path' => 'convert [file] -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB [file]',
     'png_path' => 'optipng -o4 [file]',
 ];
