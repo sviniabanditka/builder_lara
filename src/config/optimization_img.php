f12f16330869e2e5df910b4b6be2f9d0addda5be
artur <artur@vis-design.com>
2018-06-22 11:16:12 +0300

    fix optimize img

--- src/config/optimization_img.php
+++ src/config/optimization_img.php
@@ -2,6 +2,6 @@
 
 return [
     'active' => true,
-    'jpg_path' => 'convert [file] -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB [file]',
+    'jpg_path' => 'convert [file] -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG [file]',
     'png_path' => 'optipng -o4 [file]',
 ];
