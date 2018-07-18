24d5146b8e0b6d838e15b5e6c01738da55a71d1e
Artur <arturishe@gmail.com>
2018-07-18 08:15:30 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/TreeCatalogController.php
+++ src/Http/Controllers/TreeCatalogController.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Response;
 
 class TreeCatalogController
 {
