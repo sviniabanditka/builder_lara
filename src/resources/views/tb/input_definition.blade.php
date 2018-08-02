6f300c0d8cbe38a279771ea1636d7004285f8efc
artur <artur@vis-design.com>
2018-08-02 23:17:05 +0300

    fix button style click

--- src/resources/views/tb/input_definition.blade.php
+++ src/resources/views/tb/input_definition.blade.php
@@ -1,6 +1,6 @@
 <div>
     <button class="btn btn-sm btn-success" type="button"
-            onclick="ForeignDefinition.createDefinition($(this), '{{$table}}', '{{$attributes}}', {{request('id')}})" id="{{$name}}">{{__cms('Добавить')}}</button>
+            onclick="ForeignDefinition.createDefinition($(this), '{{$table}}', '{{$attributes}}', {{request('id')}})">{{__cms('Добавить')}}</button>
     <div class="loader_create_definition hide loader_definition_{{$name}}"></div>
 
     <div class="definition_blocks definition_{{$name}}">
