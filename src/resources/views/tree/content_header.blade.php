6017ca4a56f6234c49388be6e94fc89fb6f44c36
Artur <artur@vis-design.com>
2018-11-23 15:10:58 +0200

    add edit link

--- src/resources/views/tree/content_header.blade.php
+++ src/resources/views/tree/content_header.blade.php
@@ -3,3 +3,7 @@
 @foreach ($ancestors as $ancestor)
     <a href="?node={{ $ancestor->id }}" class="node_link">{{ $ancestor->title}}</a> /
 @endforeach
+
+
+<a href="javascript:void(0);" onclick="Tree.showEditForm('{{$current->id}}');" style="min-width: 70px; float: right">{{__cms('Редактировать')}}</a>
+
