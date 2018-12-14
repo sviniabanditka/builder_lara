f3f0076fee4ceac7a58509f77c686bbd24ba9897
Artur <artur@vis-design.com>
2018-12-14 11:22:36 +0200

    fix name

---
+++ src/Console/CreateImgWebp.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arturschablevsky
+ * Date: 12/14/18
+ * Time: 10:40
+ */
--- src/install/files/generateFiles/Model.php
+++ src/install/files/generateFiles/Model.php
@@ -4,6 +4,6 @@ namespace App\Models;
 
 class modelName extends BaseModel
 {
-    protected $tabe = 'tableName';
+    protected $table = 'tableName';
     protected $fillable = [];
 }
