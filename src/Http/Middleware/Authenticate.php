522fcd80c31823468d24f8d33e5fae8eb513c089
Artur <arturishe@gmail.com>
2018-12-21 14:50:10 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -21,7 +21,7 @@ class Authenticate
      */
     public function handle($request, Closure $next)
     {
-        if (!$this->checkIp($request)) {
+        if (! $this->checkIp($request)) {
             return redirect()->to('/');
         }
 
@@ -65,5 +65,4 @@ class Authenticate
 
         return true;
     }
-
 }
