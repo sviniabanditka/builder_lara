a8c216003d136430281327cc33c202af892504d7
Artur <artur@vis-design.com>
2019-03-14 18:51:26 +0200

    fix update table after save

--- src/resources/views/tb/table.blade.php
+++ src/resources/views/tb/table.blade.php
@@ -15,7 +15,7 @@
     <div class="row" style="padding-right: 13px; padding-left: 13px;">
         <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding-right: 0px; padding-left: 0px;">
             <div id="table-preloader" class="smoke_lol"><i class="fa fa-gear fa-4x fa-spin"></i></div>
-            <div class="jarviswidget jarviswidget-color-blue"
+            <div class="jarviswidget jarviswidget-color-blue" id="wid-id-1"
                 data-widget-editbutton="false"
                 data-widget-colorbutton="false"
                 data-widget-deletebutton="false"
