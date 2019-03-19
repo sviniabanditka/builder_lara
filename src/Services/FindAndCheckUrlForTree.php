3ca8d1226770a312bacffce977d9ae39d7850f25
Artur <artur@vis-design.com>
2019-03-19 14:33:08 +0200

    add class

--- src/Services/FindAndCheckUrlForTree.php
+++ src/Services/FindAndCheckUrlForTree.php
@@ -1,7 +1,76 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: arturschablevsky
- * Date: 3/19/19
- * Time: 13:36
- */
+
+namespace Vis\Builder\Services;
+
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Cache;
+
+class FindAndCheckUrlForTree
+{
+   private $model;
+
+   public function getRoute($arrSegments)
+   {
+       $this->model = config('builder.tree.model', 'App\Models\Tree');
+
+       $slug = $this->getSlug($arrSegments);
+
+       $node = $this->findUrl($slug);
+
+       if (!$node) {
+           return false;
+       }
+
+       return $this->getControllerAndMethod($node);
+   }
+
+   private function getSlug($arrSegments)
+   {
+       $slug = end($arrSegments);
+
+       if (! $slug || $slug == LaravelLocalization::setLocale()) {
+           $slug = '/';
+       }
+
+       return $slug;
+   }
+
+   private function findUrl($slug)
+   {
+       $tagsCache = config('builder.tree.cache.tags', ['tree']);
+       $model =  $this->model;
+
+       $nodes = Cache::tags($tagsCache)->rememberForever('tree_slug', function () use ($model, $slug) {
+           return $model::where('slug', 'like', $slug)->get();
+       });
+
+       foreach ($nodes as $node) {
+           if ($node->getUrl() == Request::url()) {
+               return $node;
+           }
+       }
+
+       return false;
+   }
+
+   private function getControllerAndMethod($node)
+   {
+       $templates = config('builder.tree.templates');
+
+       if (!isset($templates[$node->template])) {
+           return false;
+       }
+
+       $controllerAndMethod = explode('@', $templates[$node->template]['action']);
+
+       $app = app();
+       $controller = $app->make('App\\Http\\Controllers\\' . $controllerAndMethod[0]);
+
+       return [
+           'controller' => $controller,
+           'method' => $controllerAndMethod[1],
+           'node' => $node
+       ];
+   }
+}
