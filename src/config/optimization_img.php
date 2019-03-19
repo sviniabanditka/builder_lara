765360c98a558cab5e16817607fd50ca2886a040
Artur <artur@vis-design.com>
2019-03-19 16:17:37 +0200

    add config webp_optimize

--- src/config/optimization_img.php
+++ src/config/optimization_img.php
@@ -4,4 +4,5 @@ return [
     'active'   => true,
     'jpg_path' => 'convert [file] -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG [file]',
     'png_path' => 'optipng -o4 [file]',
+    'webp_optimize' => true
 ];
